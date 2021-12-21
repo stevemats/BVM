5f475b5f17376aca874554e75f37fc4aa8627629
stevemats <stevematindi@gmail.com>
2021-12-21 17:23:58 +0300

    minor user input & output changes

--- modules/live/live_recognizer.py
+++ modules/live/live_recognizer.py
@@ -5,20 +5,23 @@ import sys
 def live_recognizer():
 
     #read duration from the arguments
-    duration = int(input('Enter duration to record(>1):'))
+    duration = int(input('Enter duration to record in secs(>1):'))
 
-    # initialize the recognizer
+    # initializer
     r = sr.Recognizer()
     print("Please talk")
     with sr.Microphone() as source:
         # read the audio data from the default microphone
         audio_data = r.record(source, duration=duration)
         print("Recognizing...")
-        # convert speech to text
+        # convert voice to txt
         text = r.recognize_google(audio_data)
         print('Audio Recognized:',text)
+        print('\n',
+            'Your voice succeffuly converted to text(recognized_audio_(date)_(time).txt to current banja folder)')
 
-        time_now = dt.now()  # get current time of PS change
+        # //To DO: Output voice to user path of choice
+        time_now = dt.now() 
         new_time = time_now.strftime("%m-%d-%Y_%H-%M-%S")
         with open('recognized_audio_' + new_time + '.txt', 'w') as f:
             f.write(text)
